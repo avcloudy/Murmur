44e8b9f5818a1b1042c0c035b21ec9e31ca9ac33
cloudy <avcloudy@gmail.com>
2025-12-29 19:50:03 +1000

    Initial Commit

---
+++ Murmur/MurmurApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurmurApp.swift
+//  Murmur
+//
+//  Created by Tyler Hall on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MurmurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MurmurTests/MurmurTests.swift
@@ -0,0 +1,17 @@
+//
+//  MurmurTests.swift
+//  MurmurTests
+//
+//  Created by Tyler Hall on 29/12/2025.
+//
+
+import Testing
+@testable import Murmur
+
+struct MurmurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
